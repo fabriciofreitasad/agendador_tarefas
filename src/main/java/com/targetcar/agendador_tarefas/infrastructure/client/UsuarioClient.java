97936b6f32fb478b9f7972b6c114a321d89b395d
Fabricio Freitas <fabricio.freitas.ad@gmail.com>
2025-07-12 16:16:15 -0300

    Adicionadas configuraçoes de segurança

--- src/main/java/com/targetcar/agendador_tarefas/infrastructure/client/UsuarioClient.java
+++ src/main/java/com/targetcar/agendador_tarefas/infrastructure/client/UsuarioClient.java
@@ -1,4 +1,15 @@
 package com.targetcar.agendador_tarefas.infrastructure.client;
 
-public class UsuarioClient {
+import com.targetcar.agendador_tarefas.business.dto.UsuarioDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "usuario", url = "${usuario.url}")
+public interface UsuarioClient {
+
+    @GetMapping
+    UsuarioDTO buscaUsuarioPorEmail(@RequestParam("email") String email,
+                                    @RequestHeader("Authorization") String token);
 }
