8de275fbf4060467ef6dddd915e14938fb0f61e8
Fabricio Freitas <fabricio.freitas.ad@gmail.com>
2025-07-17 21:28:55 -0300

    Adicionados metodos de delete, patch de status e patch de tarefas

--- src/main/java/com/targetcar/agendador_tarefas/business/TarefaService.java
+++ src/main/java/com/targetcar/agendador_tarefas/business/TarefaService.java
@@ -2,8 +2,10 @@ package com.targetcar.agendador_tarefas.business;
 
 import com.targetcar.agendador_tarefas.business.dto.TarefasDTO;
 import com.targetcar.agendador_tarefas.business.mapper.TarefaConverter;
+import com.targetcar.agendador_tarefas.business.mapper.TarefaUpdateConverter;
 import com.targetcar.agendador_tarefas.infrastructure.entity.TarefasEntity;
 import com.targetcar.agendador_tarefas.infrastructure.enums.StatusNotificacaoEnum;
+import com.targetcar.agendador_tarefas.infrastructure.exceptions.ResourceNotFoudException;
 import com.targetcar.agendador_tarefas.infrastructure.repository.TarefasRepository;
 import com.targetcar.agendador_tarefas.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
@@ -18,9 +20,10 @@ public class TarefaService {
 
     private final TarefasRepository tarefaRepository;
     private final TarefaConverter tarefaConverter;
-    private final  JwtUtil jwtUtil;
+    private final JwtUtil jwtUtil;
+    private final TarefaUpdateConverter tarefaUpdateConverter;
 
-    public TarefasDTO gravarTarefa(String token,TarefasDTO dto) {
+    public TarefasDTO gravarTarefas(String token, TarefasDTO dto) {
         String email = jwtUtil.extrairEmailToken(token.substring(7));
 
         dto.setDataCriacao(LocalDateTime.now());
@@ -35,10 +38,40 @@ public class TarefaService {
                 tarefaRepository.findByDataEvantoBetween(dataInicial, dataFinal));
     }
 
-    public List<TarefasDTO> buscarTarefasPorEmail(String token){
+    public List<TarefasDTO> buscarTarefasPorEmail(String token) {
         String email = jwtUtil.extrairEmailToken(token.substring(7));
         return tarefaConverter.paraListaTarefasDTO(
                 tarefaRepository.findByEmailUsuario(email));
     }
 
+    public void deletaTarefaPorId(String id) {
+        try {
+            tarefaRepository.deleteById(id);
+        } catch (ResourceNotFoudException e) {
+            throw new ResourceNotFoudException("erro ao deletar tarefa por id, id enestxistente id" + id,
+                    e.getCause());
+        }
+    }
+
+    public TarefasDTO alterarStatus(StatusNotificacaoEnum status, String id) {
+        try {
+            TarefasEntity entity = tarefaRepository.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoudException("tarefa não encontrada" + id));
+            entity.setStatusNotificacaoEnum(status);
+            return tarefaConverter.paraTarefaDTO(tarefaRepository.save(entity));
+        } catch (ResourceNotFoudException e) {
+            throw new ResourceNotFoudException("Erro ao alterar status da tarefa" + e.getCause());
+        }
+    }
+
+    public TarefasDTO updatetarefas(TarefasDTO dto, String id) {
+        try {
+            TarefasEntity entity = tarefaRepository.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoudException("tarefa não encontrada" + id));
+            tarefaUpdateConverter.updateTarefas(dto, entity);
+            return tarefaConverter.paraTarefaDTO(tarefaRepository.save(entity));
+        } catch (ResourceNotFoudException e) {
+            throw new ResourceNotFoudException("Erro ao alterar status da tarefa" + e.getCause());
+        }
+    }
 }
--- src/main/java/com/targetcar/agendador_tarefas/controller/TarefasController.java
+++ src/main/java/com/targetcar/agendador_tarefas/controller/TarefasController.java
@@ -2,6 +2,7 @@ package com.targetcar.agendador_tarefas.controller;
 
 import com.targetcar.agendador_tarefas.business.TarefaService;
 import com.targetcar.agendador_tarefas.business.dto.TarefasDTO;
+import com.targetcar.agendador_tarefas.infrastructure.enums.StatusNotificacaoEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +22,7 @@ public class TarefasController {
     public ResponseEntity<TarefasDTO> gravarTarefas(
             @RequestHeader("Authorization") String token,
             @RequestBody TarefasDTO dto) {
-        return ResponseEntity.ok(tarefaService.gravarTarefa(token, dto));
+        return ResponseEntity.ok(tarefaService.gravarTarefas(token, dto));
     }
 
     @GetMapping("/eventos")
@@ -39,4 +40,19 @@ public class TarefasController {
         return ResponseEntity.ok(tarefaService.buscarTarefasPorEmail(token));
     }
 
+    @DeleteMapping
+    public ResponseEntity<Void> deletaTarefaPorId(@RequestParam("id") String id) {
+        tarefaService.deletaTarefaPorId(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<TarefasDTO> alteraStatusNotificacao(@RequestParam("status")StatusNotificacaoEnum status,
+                                                              @RequestParam("id") String id){
+        return ResponseEntity.ok(tarefaService.alterarStatus(status, id));
+    }
+    @PutMapping
+    public ResponseEntity<TarefasDTO> updatetarefas(@RequestBody TarefasDTO dto, @RequestParam("id") String id){
+        return ResponseEntity.ok(tarefaService.updatetarefas(dto,id));
+    }
 }
